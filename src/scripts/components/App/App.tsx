1ce23a6a97429319a04a0e144c6fe1ef6d9c0358
maria-kashpur <kashpur.maria@icloud.com>
2023-10-26 17:23:17 +0300

    feat: add Search component

--- src/scripts/components/App/App.tsx
+++ src/scripts/components/App/App.tsx
@@ -1,7 +1,30 @@
-import './App.css';
+import { Component } from 'react';
+import Search from '../Search/Search';
+import Cards from '../Cards/Cards';
 
-function App() {
-  return <div>React</div>;
+const categories = [
+  'characteristics',
+  'difficulty',
+  'effect',
+  'inventors',
+  'ingredients',
+  'manufacturers',
+  'name',
+  'side effects',
+  'time',
+];
+export default class App extends Component {
+  render() {
+    return (
+      <div className="content conteiner">
+        <div className="content__header">
+          <h1 className="content__title">Potions</h1>
+          <Search categories={categories} />
+        </div>
+        <div className="content__main">
+          <Cards />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/scripts/components/Cards/Cards.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class Cards extends Component {
+  render() {
+    return <div>Cards</div>;
+  }
+}
