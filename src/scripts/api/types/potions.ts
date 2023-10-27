d6d7ceb0a2e597d7049b4845b66bc9d940456979
maria-kashpur <kashpur.maria@icloud.com>
2023-10-27 09:16:34 +0300

    feat: add api

---
+++ src/scripts/api/types/potions.ts
@@ -0,0 +1,58 @@
+import { Links, Meta, Predicate } from './general';
+
+export interface APotions {
+  characteristics: string | null;
+  difficulty: string | null;
+  effect: string | null;
+  image: string | null;
+  inventors: string | null;
+  ingredients: string | null;
+  manufacturers: string | null;
+  name: string;
+  side_effects: string | null;
+  slug: string;
+  time: string | null;
+  wiki: string;
+}
+
+export type APotionsFilter =
+  | 'characteristics'
+  | 'difficulty'
+  | 'effect'
+  | 'inventors'
+  | 'ingredients'
+  | 'manufacturers'
+  | 'name'
+  | 'side_effects'
+  | 'time';
+
+export interface PotionsResponse {
+  data:
+    | {
+        id: string;
+        type: string;
+        attributes: APotions;
+        links: {
+          self: string;
+        };
+      }[]
+    | [];
+  links: Links;
+  meta: Meta;
+}
+
+export interface PotionsReqParams {
+  sort?: {
+    param: 'ASC' | 'DESC';
+    attribute: APotionsFilter;
+  };
+  pagination: {
+    limit: number;
+    page: number;
+  };
+  filters?: {
+    attribute: APotionsFilter;
+    predicate: Predicate;
+    what: string | null;
+  }[];
+}
