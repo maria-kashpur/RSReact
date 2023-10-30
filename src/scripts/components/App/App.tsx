754513b5bb3cd78284bd8e5e692f024267ceff3b
maria-kashpur <kashpur.maria@icloud.com>
2023-10-30 11:33:50 +0300

    refactor: App migrate to functional components

--- src/scripts/components/App/App.tsx
+++ src/scripts/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Search from '../Search/Search';
 import Cards from '../Cards/Cards';
 import HpApi from '../../api/HpApi';
@@ -38,24 +38,46 @@ const defaultPotionParams = {
   pagination: { limit: 30, page: 1 },
 };
 const lsPotionParams = localStorage.getItem('potionsParams');
-export default class App extends Component<unknown, IState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      items: null,
-      pagination: {
-        current: 1,
-        last: 0,
-        next: 0,
-        pages: 0,
-      },
-      params: lsPotionParams ? JSON.parse(lsPotionParams) : defaultPotionParams,
-    };
-  }
 
-  public handlePaginationClick = (btn: 'start' | 'next' | 'prev' | 'end') => {
-    let page = this.state.params.pagination.page;
+export default function App() {
+  const [state, setState] = useState<IState>({
+    isLoaded: false,
+    items: null,
+    pagination: {
+      current: 1,
+      last: 0,
+      next: 0,
+      pages: 0,
+    },
+    params: lsPotionParams ? JSON.parse(lsPotionParams) : defaultPotionParams,
+  });
+
+  const getData = async () => {
+    setState({ ...state, isLoaded: false });
+    const res = await HpApi.getPotions(state.params);
+    if (!res) return;
+    const newState = {
+      isLoaded: true,
+      items: res.data,
+      pagination: {
+        current: res.meta.pagination.current,
+        last: res.meta.pagination.last ? res.meta.pagination.last : 0,
+        next: res.meta.pagination.next ? res.meta.pagination.next : 0,
+        pages: res.meta.pagination.records
+          ? defineNumberOfPages(res.meta.pagination.records, state.params.pagination?.limit)
+          : 0,
+      },
+    };
+    setState({ ...state, ...newState });
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.params]);
+
+  const handlePaginationClick = (btn: 'start' | 'next' | 'prev' | 'end') => {
+    let page = state.params.pagination.page;
     switch (btn) {
       case 'start':
         page = 1;
@@ -67,18 +89,19 @@ export default class App extends Component<unknown, IState> {
         page = page + 1;
         break;
       case 'end':
-        page = this.state.pagination.pages;
+        page = state.pagination.pages;
         break;
       default:
         break;
     }
-    const newParams = { ...this.state.params };
+    const newParams = { ...state.params };
     newParams.pagination.page = page;
-    this.setState({ params: newParams });
+    setState({ ...state, params: newParams });
   };
 
-  public handleSendParams = async (filter: PotionsReqParams['filters']) => {
-    this.setState((state) => ({
+  const handleSendParams = async (filter: PotionsReqParams['filters']) => {
+    setState({
+      ...state,
       params: {
         filters: filter,
         pagination: {
@@ -86,66 +109,31 @@ export default class App extends Component<unknown, IState> {
           limit: state.params.pagination.limit,
         },
       },
-    }));
+    });
   };
 
-  public async getData() {
-    this.setState({ isLoaded: false });
-    const res = await HpApi.getPotions(this.state.params);
-    if (!res) return;
-    this.setState((state) => ({
-      items: res.data,
-      pagination: {
-        current: res.meta.pagination.current,
-        last: res.meta.pagination.last ? res.meta.pagination.last : 0,
-        next: res.meta.pagination.next ? res.meta.pagination.next : 0,
-        pages: res.meta.pagination.records
-          ? defineNumberOfPages(res.meta.pagination.records, state.params.pagination?.limit)
-          : 0,
-      },
-    }));
-    this.setState({ isLoaded: true });
-  }
-
-  componentDidMount(): void {
-    this.getData();
-  }
-
-  componentDidUpdate(_prevProps: unknown, prevState: { params: PotionsReqParams }) {
-    if (prevState.params !== this.state.params) {
-      this.getData();
-      localStorage.setItem('potionsParams', JSON.stringify(this.state.params));
-    }
-  }
-
-  render() {
-    const template = (
-      <div className="content conteiner">
-        <div className="content__header">
-          <h1 className="content__title">Potions</h1>
-          <BtnError />
-          <Search
-            categories={categories}
-            params={this.state.params}
-            hundleSendParams={this.handleSendParams.bind(this)}
-          />
-        </div>
-        <div className="content__main">
-          <Pagination
-            pagination={this.state.pagination}
-            handlePaginationClick={this.handlePaginationClick}
-            params={this.state.params.pagination}
-          />
-          {this.state.items ? <Cards data={this.state.items} /> : <div>get data...</div>}
-        </div>
+  const template = (
+    <div className="content conteiner">
+      <div className="content__header">
+        <h1 className="content__title">Potions</h1>
+        <BtnError />
+        <Search categories={categories} params={state.params} hundleSendParams={handleSendParams} />
       </div>
-    );
+      <div className="content__main">
+        <Pagination
+          pagination={state.pagination}
+          handlePaginationClick={handlePaginationClick}
+          params={state.params.pagination}
+        />
+        {state.items ? <Cards data={state.items} /> : <div>get data...</div>}
+      </div>
+    </div>
+  );
 
-    return (
-      <>
-        {this.state.isLoaded ? '' : <Loader />}
-        {template}
-      </>
-    );
-  }
+  return (
+    <>
+      {state.isLoaded ? '' : <Loader />}
+      {template}
+    </>
+  );
 }
