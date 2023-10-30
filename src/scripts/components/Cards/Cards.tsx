2f6c693832fa366d1b7a8797726c5bb11f490432
maria-kashpur <kashpur.maria@icloud.com>
2023-10-30 10:41:22 +0300

    refactor: Cards migrate to functional components

--- src/scripts/components/Cards/Cards.tsx
+++ src/scripts/components/Cards/Cards.tsx
@@ -1,27 +1,19 @@
-import { Component } from 'react';
 import { PotionsResponse } from '../../api/types/potions';
 import './cards.scss';
 import CardPotion from './CardPotion/CardPotion';
-interface IState {}
 
 interface IProps {
   data: PotionsResponse['data'];
 }
 
-export default class Cards extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="cards">
-        {this.props.data.map((el, index) => (
-          <div className="cards_item" key={`card_${index}`}>
-            <CardPotion cardData={el} />
-          </div>
-        ))}
-      </div>
-    );
-  }
+export default function Cards({ data }: IProps) {
+  return (
+    <div className="cards">
+      {data.map((el, index) => (
+        <div className="cards_item" key={`card_${index}`}>
+          <CardPotion cardData={el} />
+        </div>
+      ))}
+    </div>
+  );
 }
