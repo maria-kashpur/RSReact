d41a9494940143ce651be59b4757fc1f64126357
maria-kashpur <kashpur.maria@icloud.com>
2023-11-02 00:06:01 +0300

    feat:  add needed settings for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,9 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  settings: { react: { version: '18.2' } },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
