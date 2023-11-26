11476ec0bfe152611d7c9a4d0ad12b41572d2902
maria-kashpur <kashpur.maria@icloud.com>
2023-11-26 22:27:05 +0300

    feat: add NotFoundPage

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -4,14 +4,16 @@ import img from '../../assets/images/harry-potter-4077473_1280.png';
 
 export default function ErrorPage() {
   return (
-    <div className={s.not_found}>
-      <div className={s.not_found__box}>
-        <div className={s.not_found__item}></div>
-        <div className={s.not_found__item}>
-          <Image src={img} alt="error icon" height={519} width={510} priority={true}></Image>
+    <>
+      <div className={s.not_found}>
+        <div className={s.not_found__box}>
+          <div className={s.not_found__item}></div>
+          <div className={s.not_found__item}>
+            <Image src={img} alt="error icon" height={519} width={510} priority={true}></Image>
+          </div>
         </div>
+        <div className={s.not_found__messege}>The error was raised and handled</div>
       </div>
-      <div className={s.not_found__messege}>The error was raised and handled</div>
-    </div>
+    </>
   );
 }
